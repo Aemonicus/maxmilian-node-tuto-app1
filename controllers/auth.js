ed0f189036d744dc27f1ba8cc6e2ceb5b51b8928
Florent VICENTE <vicentegroupe@gmail.com>
2021-11-26 15:21:33 +0100

    sessions & cookiesé

---
+++ controllers/auth.js
@@ -0,0 +1,29 @@
+const User = require('../models/user');
+
+exports.getLogin = (req, res, next) => {
+  res.render('auth/login', {
+    path: '/login',
+    pageTitle: 'Login',
+    isAuthenticated: false
+  });
+};
+
+exports.postLogin = (req, res, next) => {
+  User.findById('619d2422a820e31963f43e5e')
+    .then(user => {
+      req.session.isLoggedIn = true;
+      req.session.user = user;
+      req.session.save(err => {
+        console.log(err);
+        res.redirect('/');
+      });
+    })
+    .catch(err => console.log(err));
+};
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy(err => {
+    console.log(err);
+    res.redirect('/');
+  });
+};
